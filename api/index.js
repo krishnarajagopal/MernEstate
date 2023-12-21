3ab72f1beba0155e55315aa6805d978c9570ff75
Krishna Rajagopal <krishan.rajagopal@gmail.com>
2023-12-21 15:33:38 -0800

    added backend server

---
+++ api/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+
+
+const app = express();
+
+app.get("/", (req, res) => {
+        
+})
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
