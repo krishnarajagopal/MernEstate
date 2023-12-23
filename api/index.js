a9351bf63f566c45fab147700fab85c920187664
Krishna Rajagopal <krishan.rajagopal@gmail.com>
2023-12-22 22:13:20 -0800

    Created Test Api Routes

--- api/index.js
+++ api/index.js
@@ -1,19 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+
 dotenv.config();
-mongoose.connect(process.env.MONGO_URI).then(() => {
-        console.log("Connected to MongoDB!");
-}).catch((err) => {
-        console.log(err);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB!!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.get("/", (req, res) => {
-        
-})
-
 app.listen(3000, () => {
-    console.log("Listening on port 3000 ...");
+  console.log("Listening on port 3000 ...");
 });
+
+app.use("/api/user", userRoute);
