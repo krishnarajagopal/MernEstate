9e61d87eabc0ded02721534e3520ad50d70ff0eb
Krishna Rajagopal <krishan.rajagopal@gmail.com>
2023-12-26 10:16:15 -0800

    created auth-signup route with bcrypt encryption

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -3,7 +3,7 @@ const UserSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: password, required: true },
+    password: { type: String, required: true },
   },
   {
     timestamps: true,
