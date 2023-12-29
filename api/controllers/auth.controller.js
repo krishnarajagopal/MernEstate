3f50069e3a7192316bf2ef02edd5869e7e8f91c6
Krishna Rajagopal <krishan.rajagopal@gmail.com>
2023-12-29 10:51:56 -0800

    Created signin api route in back end in auth.controller.js and tested to return access token cookie and status message

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,39 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const signup = async(req, res, next) => {
-        const { username, email, password } = req.body;
-        const hashedPassword = bcrypt.hashSync(password, 10);
-        const newUser = new User({ username, email, password: hashedPassword });
-       try {
-        await newUser.save()
-        res.status(201).json({ message: "User Created Successfully" });
-       } catch (error) {
-        next(error);   
-       }
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User Created Successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
 
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return next(errorHandler(404, "Invalid email or password"));
+    }
+    const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+    if (!isPasswordCorrect) {
+      return next(errorHandler(400, "Invalid email or password"));
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...otherUserInfo } = user._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true })
+      .status(200)
+      .json({ message: "Login Successful", otherUserInfo });
+  } catch (error) {
+    next(error);
+  }
 };
--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
 
 dotenv.config();
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
@@ -14,15 +15,21 @@ mongoose
     console.log(err);
   });
 
-const app = express();
+//use express and express json
+  const app = express();
 app.use(express.json());
 
+// Listen @ port 3000
 app.listen(3000, () => {
   console.log("Listening on port 3000 ...");
 });
 
+// Routes
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
+
+
+// Error Handling Middleware
 app.use((err, req, res, next) => {
   const statuscode = err.statusCode ? err.statusCode : 500;
   const message = err.message ? err.message : "Internal Server Error";
