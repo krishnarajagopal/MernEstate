a42648385f12563c823a1a56af4d224792b701bb
Krishna Rajagopal <krishan.rajagopal@gmail.com>
2024-01-02 16:42:21 -0800

    created the redux tool kit to manage the state globally and added dipatch functions in appropriate places in SignIn.jsx

--- Client/src/pages/SignIn.jsx
+++ Client/src/pages/SignIn.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice.js";
 
 export default function SignIp() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { error, loading } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -15,7 +17,7 @@ export default function SignIp() {
   const handlesubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -26,17 +28,13 @@ export default function SignIp() {
       const data = await res.json();
       console.log("data", data);
       if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data.data));
       navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
-      // console.log(error);
+      dispatch(signInFailure(error.message));
     }
   };
 
